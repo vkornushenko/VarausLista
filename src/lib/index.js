2043365f8ab6bbdd9590c2da1b3cd78c2e1b37ea
Vladislav Korniushenko <vkornushenko@gmail.com>
2024-01-04 12:55:23 +0200

    delete old redux store files

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import uiSlice from "./ui_slice";
-
-export const makeStore = configureStore({
-    reducer: {ui: uiSlice.reducer}
-});
-
-// export default store;
