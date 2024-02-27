4a6ba95365502b80b5f7ee911e76cd09a47a1dda
Vladislav Korniushenko <vkornushenko@gmail.com>
2024-02-27 15:11:38 +0200

    test auth cookies issue (build error fixed)

--- src/app/utils/supabase/server.js
+++ src/app/utils/supabase/server.js
@@ -2,7 +2,7 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = (cookieStore) => {
-  const cookieStore = cookies();
+  // const cookieStore = cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
